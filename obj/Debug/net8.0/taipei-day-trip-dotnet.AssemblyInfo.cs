6af74ce21a993736a31419350b8b163983736279
chun <chun.yii.chen@gmail.com>
2025-04-28 02:16:53 +0800

    modify

--- obj/Debug/net8.0/taipei-day-trip-dotnet.AssemblyInfo.cs
+++ obj/Debug/net8.0/taipei-day-trip-dotnet.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("taipei-day-trip-dotnet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ddb00dbfc6de5906e5022b21c1183d44583fcd5")]
 [assembly: System.Reflection.AssemblyProductAttribute("taipei-day-trip-dotnet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("taipei-day-trip-dotnet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
